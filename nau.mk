30021a2bb4465e5d739e7bf90cb840e8601c38ce
Luis Moreno <luis.moreno@edunext.co>
2019-05-25 22:00:06 -0400

    NAU118: Custom commands for statics compilation

---
+++ nau.mk
@@ -0,0 +1,6 @@
+lms-static-sass:
+	docker exec -t edx.devstack.lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform/ && paver update_assets lms --skip-collect'
+cms-static-sass:
+	docker exec -t edx.devstack.studio bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform/ && paver update_assets cms --skip-collect'
+
+
